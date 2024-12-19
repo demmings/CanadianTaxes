bd0dd4d6d48fcd65c1fb94dc0f0250a6600a6645
cdemmigs <cdemmings@gmail.com>
2024-12-19 13:46:58 -0500

    Updated TEST_CANTAX.  Added 2025 smoke test.

--- dist/CanadianTaxes.js
+++ dist/CanadianTaxes.js
@@ -1,52 +1,99 @@
 
-const TestCantaxData =
-    [
-        [59, 180000.00, 300244.00, 5000.00, 0.00, 0.00, 0.00, 300243.70, 180000.35, -0.30, 0.35],
-        [60, 57000.00, 70272.00, 0.00, 0.00, 0.00, 0.00, 70271.74, 57000.22, -0.26, 0.22],
-        [61, 58000.00, 72833.00, 10200.00, 0.00, 0.00, 0.00, 72833.04, 58000.14, 0.04, 0.14],
-        [62, 59000.00, 74277.00, 10450.00, 0.00, 0.00, 0.00, 74277.23, 59000.02, 0.23, 0.02],
-        [63, 60000.00, 75712.00, 10600.00, 0.00, 0.00, 0.00, 75712.35, 59999.96, 0.35, -0.04],
-        [64, 61000.00, 77191.00, 10800.00, 0.00, 0.00, 0.00, 77190.78, 61000.20, -0.22, 0.20],
-        [65, 62000.00, 78368.00, 11000.00, 0.00, 0.00, 0.00, 78368.48, 61999.84, 0.48, -0.16],
-        [66, 70000.00, 90663.00, 12000.00, 0.00, 0.00, 0.00, 90662.85, 70000.21, -0.15, 0.21],
-        [67, 80000.00, 108254.00, 12800.00, 0.00, 0.00, 0.00, 108254.04, 80000.05, 0.04, 0.05],
-        [68, 90000.00, 126016.00, 13000.00, 0.00, 0.00, 0.00, 126014.68, 90000.88, -1.32, 0.88],
-        [69, 90000.00, 120185.00, 0.00, 0.00, 0.00, 0.00, 120185.01, 90000.23, 0.01, 0.23],
-        [70, 120000.00, 173897.00, 0.00, 0.00, 0.00, 0.00, 173896.61, 120000.33, -0.39, 0.33],
-        [70, 120000.00, 178565.00, 0.00, 10000.00, 0.00, 0.00, 178565.03, 120000.09, 0.03, 0.09],
-        [70, 120000.00, 224492.00, 0.00, 100000.00, 0.00, 0.00, 224491.27, 120000.47, -0.73, 0.47],
-        [70, 120000.00, 502868.00, 0.00, 500000.00, 0.00, 0.00, 502868.57, 119999.80, 0.57, -0.20],
-        [70, 120000.00, 511335.00, 10000.00, 500000.00, 0.00, 0.00, 511334.99, 120000.10, -0.01, 0.10],
-        [70, 120000.00, 319350.00, 10000.00, 249999.00, 0.00, 0.00, 319350.03, 120000.08, 0.03, 0.08],
-        [70, 50000.00, 71924.00, 50000.00, 0.00, 0.00, 0.00, 71923.93, 50000.17, -0.07, 0.17],
-        [70, 10000.00, 10796.00, 50000.00, 0.00, 0.00, 0.00, 10795.85, 10000.32, -0.15, 0.32],
-        [70, 0.00, 600.00, 50000.00, 0.00, 0.00, 0.00, 600.01, 0.00, 0.01, 0.00],
-        //  Known fails. Low income tax credits not calculated.
-        //[70, 0.00, 621.00, 0.00, 50000.00, 0.00, 0.00, 883.58, -209.88, 262.58, -209.88],
-        //[70, 0.00, 621.00, 0.00, 49999.00, 0.00, 0.00, 883.46, -209.78, 262.46, -209.78],
-        //[70, 0.00, 621.00, 0.00, 50001.00, 0.00, 0.00, 883.71, -209.98, 262.71, -209.98],
-        [70, 180000.00, 309659.00, 5000.00, 0.00, 9416.00, 0.00, 320505.23, 179999.72, 0.23, -0.28],
-        [70, 80000.00, 108410.00, 5000.00, 0.00, 9416.00, 0.00, 118561.07, 80000.07, 0.07, 0.07],
-        [70, 50000.00, 59398.00, 5000.00, 0.00, 9416.00, 0.00, 64187.34, 49999.90, 0.34, -0.10],
-        [70, 50000.00, 58824.00, 5000.00, 0.00, 9416.00, 2000.00, 63613.38, 49999.91, 0.38, -0.09],
-        [70, 50000.00, 59309.00, 5000.00, 2000.00, 9416.00, 2000.00, 63613.38, 49999.91, 0.38, -0.09]
-    ];
-
 /**
  * Basic sanity check.
  * @returns {any[]}
  * @customfunction
  */
 function TEST_CANTAX() {
-    const output = [["Age", "Net Income", "Dividends", "Eligible Capital Gains", "OAS", "Pension Credit Eligible Income", "Gross Income (calculated)", "Expected Gross", "Gross Diff. (calc-expected)", "Pass/Fail", "Net Income (calculated)", "Gross Diff. (calc-expected)", "Pass/Fail"]];
-    for (const testItem of TestCantaxData) {
-        const grossIncome = GET_GROSS_INCOMES_V2(testItem[1], testItem[0], testItem[0], 0, 2024, testItem[4], testItem[3], testItem[5], testItem[6], 0, 0, 0);
-        const netIncome = GET_NET_INCOMES_V2(testItem[2], testItem[0], testItem[0], 0, 2024, testItem[4], testItem[3], testItem[5], testItem[6], 0, 0);
+    const TestCantaxData = [
+        ["Tax Year","Age","Net Income","Expected Gross Taxable Income","Dividends Eligible for Enhanced Tax Credit","Dividends (non elibible)","Eligible Capital Gains","OAS","Pension Credit Eligible Income","Medical Expenses","Charitable Donations"],
+        [2024,59,180000.00,300244.00,5000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,60,57000.00,70272.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,61,58000.00,72833.00,10200.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,62,59000.00,74277.00,10450.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,63,60000.00,75712.00,10600.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,64,61000.00,77191.00,10800.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,65,62000.00,78368.00,11000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,66,70000.00,90663.00,12000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,67,80000.00,108254.00,12800.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,68,90000.00,126016.00,13000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,69,90000.00,120185.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,70,120000.00,173897.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,70,120000.00,178565.00,0.00,0.00,10000.00,0.00,0.00,0.00,0.00],
+        [2024,70,120000.00,224492.00,0.00,0.00,100000.00,0.00,0.00,0.00,0.00],
+        [2024,70,120000.00,502868.00,0.00,0.00,500000.00,0.00,0.00,0.00,0.00],
+        [2024,70,120000.00,511335.00,10000.00,0.00,500000.00,0.00,0.00,0.00,0.00],
+        [2024,70,120000.00,319350.00,10000.00,0.00,249999.00,0.00,0.00,0.00,0.00],
+        [2024,70,50000.00,71924.00,50000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,70,10000.00,10796.00,50000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,70,0.00,600.00,50000.00,0.00,0.00,0.00,0.00,0.00,0.00],
+        [2024,70,180000.00,309659.00,5000.00,0.00,0.00,9416.00,0.00,0.00,0.00],
+        [2024,70,80000.00,108410.00,5000.00,0.00,0.00,9416.00,0.00,0.00,0.00],
+        [2024,70,50000.00,59398.00,5000.00,0.00,0.00,9416.00,0.00,0.00,0.00],
+        [2024,70,50000.00,58824.00,5000.00,0.00,0.00,9416.00,2000.00,0.00,0.00],
+        [2024,70,50000.00,59309.00,5000.00,0.00,2000.00,9416.00,2000.00,0.00,0.00],
+        [2024,70,50000.00,58713.00,5000.00,0.00,2000.00,9416.00,2000.00,4000.00,0.00],
+        [2025,62,50000.00,59766.00,5000.00,0.00,2000.00,0.00,2000.00,4000.00,0.00],
+        [2025,70,50000.00,58254.00,5000.00,0.00,2000.00,9416.00,2000.00,4000.00,0.00],
+        [2025,69,80000.00,135075.00,50000.00,0.00,2000.00,9416.00,2000.00,4000.00,0.00],
+        [2025,69,80000.00,151965.00,50000.00,20000.00,2000.00,9416.00,2000.00,4000.00,0.00],
+        [2025,69,80000.00,151133.00,50000.00,20000.00,2000.00,9416.00,2000.00,4000.00,1000.00]
+    ];
 
-        const grossDiff = Math.round(grossIncome[0][0]) - testItem[2];
-        const netDiff = Math.round(netIncome[0][0]) - testItem[1];
+    const calcColums = [ "Gross Income (calculated)", "Gross Diff", "Pass/Fail", "Net Income (calculated)",  "Net Diff", "Pass/Fail"];
+    const output = [[...TestCantaxData[0], ...calcColums]];
+    const EXPECTED_GROSS_COL = TestCantaxData[0].indexOf("Expected Gross Taxable Income");
+    const FUTURE_AGE_COL = TestCantaxData[0].indexOf("Age");
+    const NET_INCOME_COL = TestCantaxData[0].indexOf("Net Income");
+    const YEAR_COL = TestCantaxData[0].indexOf("Tax Year");
+    const GAINS_COL = TestCantaxData[0].indexOf("Eligible Capital Gains");
+    const ELIG_DIV_COL = TestCantaxData[0].indexOf("Dividends Eligible for Enhanced Tax Credit");
+    const OAS_COL = TestCantaxData[0].indexOf("OAS");
+    const PENSION_COL = TestCantaxData[0].indexOf("Pension Credit Eligible Income");
+    const MEDICAL_COL = TestCantaxData[0].indexOf("Medical Expenses");
+    const NON_ELIG_DIV_COL = TestCantaxData[0].indexOf("Dividends (non elibible)");
+    const CHARITY_COL = TestCantaxData[0].indexOf("Charitable Donations");
+
+
+    for (let i = 1; i < TestCantaxData.length; i++) {
+        const testItem = TestCantaxData[i];
+
+        const grossIncome = GET_GROSS_INCOMES_V2(testItem[NET_INCOME_COL],  // Income
+            testItem[FUTURE_AGE_COL],                                       // Future age
+            Number(testItem[FUTURE_AGE_COL]),                               // Current age
+            0,                                                              // Inflation
+            Number(testItem[YEAR_COL]),                                     // Tax year
+            testItem[GAINS_COL],                                            // Capital gains
+            testItem[ELIG_DIV_COL],                                         // Eligible Dividends
+            testItem[OAS_COL],                                              // OAS
+            testItem[PENSION_COL],                                          // Pension credit income 
+            testItem[MEDICAL_COL],                                          // Medical expenses
+            testItem[NON_ELIG_DIV_COL],                                     // Non eligible dividends
+            testItem[CHARITY_COL]                                           // Charitable donations
+        );
+        
+        const netIncome = GET_NET_INCOMES_V2(testItem[EXPECTED_GROSS_COL],  // Income
+            testItem[FUTURE_AGE_COL],                                       // Future age
+            Number(testItem[FUTURE_AGE_COL]),                               // Current age
+            0,                                                              // Inflation
+            Number(testItem[YEAR_COL]),                                     // Tax year
+            testItem[GAINS_COL],                                            // Capital gains
+            testItem[ELIG_DIV_COL],                                         // Eligible Dividends
+            testItem[OAS_COL],                                              // OAS
+            testItem[PENSION_COL],                                          // Pension credit income 
+            testItem[MEDICAL_COL],                                          // Medical expenses
+            testItem[NON_ELIG_DIV_COL],                                     // Non eligible dividends
+            testItem[CHARITY_COL]                                           // Charitable donations
+        );
+
+        const grossDiff = Math.round(grossIncome[0][0]) - Number(testItem[EXPECTED_GROSS_COL]);
+        const grossStatus = Math.abs(grossDiff) <= 1.0 ? "Pass" : "** FAIL **";
+         
+        const netDiff = Math.round(netIncome[0][0]) - Number(testItem[NET_INCOME_COL]);
+        const netStatus = Math.abs(netDiff) <= 1.0 ? "Pass" : "** FAIL **";
+        const calculatedData = [grossIncome[0][0], grossDiff, grossStatus, netIncome[0][0], netDiff, netStatus];
         // @ts-ignore
-        output.push([testItem[0], testItem[1], testItem[3], testItem[4], testItem[5], testItem[6], Math.round(grossIncome[0][0]), testItem[2], grossDiff, Math.abs(grossDiff) <= 1.0 ? "Pass" : "** FAIL **", Math.round(netIncome[0][0]), netDiff, Math.abs(netDiff) <= 1.0 ? "Pass" : "** FAIL **"]);
+        output.push([...testItem, ...calculatedData]);
     }
 
     return output;
